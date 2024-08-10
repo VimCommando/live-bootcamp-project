5f13db4f037c9da9579ddd93947777b248c5a7ab
Ryan Eno <ryan.eno@elastic.co>
2024-08-09 21:31:43 -0700

    [Sprint 3][Task 1] Implement log-in route error cases

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,5 +1,40 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use crate::app_state::AppState;
+use crate::domain::{AuthAPIError, Email, Password, UserStore, UserStoreError};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use serde::Deserialize;
 
-pub async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+pub async fn login(
+    State(state): State<AppState>,
+    Json(request): Json<LoginRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email = match Email::parse(&request.email) {
+        Ok(email) => email,
+        Err(_) => return Err(AuthAPIError::IncorrectCredentials),
+    };
+
+    let password = match Password::parse(&request.password) {
+        Ok(password) => password,
+        Err(_) => return Err(AuthAPIError::IncorrectCredentials),
+    };
+
+    let user_store = state.user_store.read().await;
+    match user_store.validate_user(&email, &password).await {
+        Ok(_) => (),
+        Err(UserStoreError::UserNotFound) => return Err(AuthAPIError::IncorrectCredentials),
+        Err(UserStoreError::InvalidCredentials) => return Err(AuthAPIError::IncorrectCredentials),
+        Err(_) => return Err(AuthAPIError::UnexpectedError),
+    };
+
+    let user = match user_store.get_user(&email).await {
+        Ok(user) => user,
+        Err(_) => return Err(AuthAPIError::UnexpectedError),
+    };
+
+    Ok(StatusCode::OK.into_response())
+}
+
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,10 +1,51 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
+use auth_service::ErrorResponse;
+use serde_json::json;
 
 #[tokio::test]
-async fn login_returns_200() {
+async fn should_return_422_if_malformed_credentials() {
     let app = TestApp::new().await;
+    let body = json!({
+        "email": get_random_email(),
+    });
 
-    let response = app.post_login().await;
+    let response = app.post_login(&body).await;
 
-    assert_eq!(response.status().as_u16(), 200);
+    assert_eq!(response.status().as_u16(), 422);
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+    let body = json!({
+        "email": "IDontExist@gmail.com",
+        "password": "InvalidPassword"
+    });
+
+    let response = app.post_login(&body).await;
+
+    assert_eq!(response.status().as_u16(), 400);
+}
+
+#[tokio::test]
+async fn should_return_401_if_incorrect_credentials() {
+    let app = TestApp::new().await;
+    let user_json = json!({
+        "email": "mreynolds@serenity.co",
+        "password": "N0thingInTheverse!",
+        "requires2FA": false
+    });
+
+    let response = app.post_signup(&user_json).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+
+    let invalid_password = json!({
+        "email": "mreynolds@serenity.co",
+        "password": "Noth1ngInTheverse?",
+    });
+
+    let response = app.post_login(&invalid_password).await;
+
+    assert_eq!(response.status().as_u16(), 401);
 }
